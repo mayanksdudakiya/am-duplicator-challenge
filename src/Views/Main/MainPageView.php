9aee5bd6ccb43d2b5435f32f0b48c178e68616c4
mayanksdudakiya <mayanksdudakiya@gmail.com>
2024-10-30 15:30:09 +0530

    feat: scan button added in main page view

--- src/Views/Main/MainPageView.php
+++ src/Views/Main/MainPageView.php
@@ -16,10 +16,12 @@ class MainPageView
             <h1>
                 <?php echo esc_html(get_admin_page_title()); ?>
             </h1>
-            
+
             <p>
                 <?php _e('Here is the main page', 'dup-challenge'); ?>
             </p>
+
+            <button class="button action" id="btn-directory-and-files-scan">Scan</button>
         </div>
         <?php
     }

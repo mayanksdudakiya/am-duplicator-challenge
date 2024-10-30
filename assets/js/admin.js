320e9784020f89e08e962443de3f555cc82ab59e
mayanksdudakiya <mayanksdudakiya@gmail.com>
2024-10-30 15:32:20 +0530

    js: on scan button click call PHP function with nonce check

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,1 +1,29 @@
-/* SCRIPTS */
+jQuery(document).ready(function ($) {
+    $('#btn-directory-and-files-scan').on('click', function (event) {
+        event.stopPropagation();
+
+        jQuery.ajax({
+            type: "POST",
+            url: duplicator_admin_script_data.ajax_url,
+            dataType: "json",
+            data: {
+                action: 'duplicator_scan_directories_and_files',
+                nonce: duplicator_admin_script_data.duplicator_scan_directories_and_files_nonce,
+            },
+            success: function (result, textStatus, jqXHR) {
+                console.log('success called');
+                console.log(result);
+                // if (result.success) {
+                //     redirectToRemoteEndpoint(result.data.funcData);
+                // } else {
+                //     addErrorMessage(result.data.message);
+                // }
+            },
+            error: function (result, textStatus, error) {
+                console.log('error called');
+                console.log(result);
+                console.log(error);
+            }
+        });
+    });
+});
